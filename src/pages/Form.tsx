026607f3c7979a312634f6e4866b80f61a638fda
Anderson Ferreira <andersonferreira251139@gmail.com>
2024-07-09 16:09:16 -0300

    up

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -31,7 +31,7 @@ const Form = () => {
       ) : (
         <div className="container-app">
           <div className="container-header">
-            <h1>Sign In</h1>
+            <h1>Register</h1>
             <span>Crie sua conta para acessar nossos recursos e serviços.</span>
           </div>
           <form onSubmit={handleSubmit(onSubmit)} className="container-form">
--- src/pages/Thanks.tsx
+++ src/pages/Thanks.tsx
@@ -1,13 +1,22 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import "../styles.css";
 
 const Thanks = () => {
   const location = useLocation();
   const { user } = location.state || {};
+  const navigate = useNavigate()
+
   return (
-    <div>
-      <h1>Seja bem vindo {user.name}</h1>
-      <h2>obrigado por se cadastrar em nosso sistema!</h2>
-      <p>aproveite nossos recursos e se divirta</p>
+    <div className="container-thanks">
+      <div className="content-thanks">
+        <button onClick={() => navigate(-1)}>Voltar ao formulario</button>
+        <h1>
+          Seja bem vindo,
+          <span>{user.name}!</span>
+        </h1>
+        <h4>obrigado por se cadastrar em nosso sistema!</h4>
+        <p>aproveite nossos recursos e se divirta.</p>
+      </div>
     </div>
   );
 };
